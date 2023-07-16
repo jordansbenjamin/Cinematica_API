5946b327da9ad55f8aad12b6b16618d00e7a1daf
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-16 15:59:25 +1000

    Added movie_log db model

---
+++ src/models/movie_log.py
@@ -0,0 +1,14 @@
+from main import db
+from datetime import datetime
+
+class MovieLog(db.Model):
+    # Table name for db
+    __tablename__ = 'movie_logs'
+
+    # PK for each movie entry in the movie log
+    id = db.Column(db.Integer(), primary_key=True)
+    # Date movie is added to the movie log
+    date_watched = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
+
+    # Establishing relationships:
+    # WILL DO LATER
