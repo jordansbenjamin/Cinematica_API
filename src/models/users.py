fab558ba7ee91124b0c7642a5b5d643aabb7b717
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-16 14:52:42 +1000

    Updated user model

--- src/models/users.py
+++ src/models/users.py
@@ -1,19 +1,27 @@
 from main import db
 from datetime import datetime
 
+
 class User(db.Model):
+    # NOTE:
+    # Class attrbute to associate model class with db table
+    # Considered good practice to include
+    # Otherwise SQLAlchemy will automatically generate it
+    # Converts PascalCase class model name to snake_case db name
+
+    # Table name for db
     __tablename__ = 'users'
 
+    # Sets the user's PK
     id = db.Column(db.Integer(), primary_key=True)
-
     # User's email
     email = db.Colum(db.String(128), nullable=False, unique=True)
-
     # User's username
     username = db.Column(db.String(64), nullable=False, unique=True)
-
     # User's password
     password = db.Column(db.String(64), nullable=False)
-
-    # User's join date
+    # User's date joined
     join_date = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    # Establishing relationships:
+    # WILL DO AFTER OTHER MODEL ARE CREATED FIRST
