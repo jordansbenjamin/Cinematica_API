b8a563b02cd73386c45a25d8a445b96676cde1af
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-16 11:30:16 +1000

    Added configuration to main.py file, added more requirements and set the flaskenv file

---
+++ src/main.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+db = SQLAlchemy()
+ma = Marshmallow()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
+
+def create_app():
+    # Creating the flask app object - this is the core of the app!
+    app = Flask(__name__)
+
+    # configuring our app:
+    app.config.from_object("config.app_config")
+
+    # creating our database object! This allows the use of ORM
+    db.init_app(app)
+
+    # creating our marshmallow object! This allows the use of schemas
+    ma.init_app(app)
+
+    # creating the jwt and bcrypt objects! this allows the use of authentication
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    # WILL ACTIVATE LATER
+    # from commands import db_commands
+    # app.register_blueprint(db_commands)
+
+    # # import the controllers and activate the blueprints
+    # from controllers import registerable_controllers
+
+    # for controller in registerable_controllers:
+    #     app.register_blueprint(controller)
+
+    return app
