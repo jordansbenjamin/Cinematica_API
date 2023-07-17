b87a7c98490ffba5675a69fb011d1a9eedfe196d
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-17 12:05:41 +1000

    Created __init__ file with existing controllers so far and added controller registration in main.py

---
+++ src/controllers/__init__.py
@@ -0,0 +1,10 @@
+from user_controller import users
+from movie_controller import movies
+from watchlist_controller import watchlists
+
+registerable_controllers = [
+    users,
+    movies,
+    watchlists,
+    # Rest will be added later
+]
