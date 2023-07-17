adebe018ab4a72e247b7cf77310d6cc552c51217
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-17 11:48:48 +1000

    Created watchlist controller

---
+++ src/controllers/watchlist_controller.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, jsonify
+from models.watchlist import Watchlist
+from schemas.watchlist_schema import watchlist_schema
+
+# Initialises flask blueprint with a /watchlists url prefix
+watchlists = Blueprint('watchlists', __name__, url_prefix="/watchlists")
+
+
+@watchlists.route("/", methods=["GET"])
+def get_watchlists(user_id):
+    '''GET endpoint/handler for fetching specified users watchlist available in the cinematica app'''
+    # Query all watchlist instances from the DB
+    watchlists = Watchlist.query.filter_by(user_id=user_id)
+    # Serialises queried watchlist instances from DB with marshmallow schema into Python DST
+    result = watchlist_schema.dump(watchlists)
+    # Returns the serialised data into JSON format for response
+    return jsonify(result)
