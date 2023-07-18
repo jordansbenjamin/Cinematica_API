169abef83dec9a7510202331d7c058844f440fec
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-18 12:51:50 +1000

    Added nullable=False constraint on association tables

--- src/models/associations.py
+++ src/models/associations.py
@@ -2,7 +2,7 @@ from main import db
 
 
 watchlist_movie_association = db.Table('watchlist_movie_association', db.Column(
-    'movielog_id', db.Integer, db.ForeignKey('watchlists.id')), db.Column('movie_id', db.Integer, db.ForeignKey('movies.id')))
+    'movielog_id', db.Integer, db.ForeignKey('watchlists.id'), nullable=False), db.Column('movie_id', db.Integer, db.ForeignKey('movies.id'), nullable=False))
 
 movielog_movie_association = db.Table('movielog_movie_association', db.Column(
-    'movielog_id', db.Integer, db.ForeignKey('movielogs.id')), db.Column('movie_id', db.Integer, db.ForeignKey('movies.id')))
+    'movielog_id', db.Integer, db.ForeignKey('movielogs.id'), nullable=False), db.Column('movie_id', db.Integer, db.ForeignKey('movies.id'), nullable=False))
