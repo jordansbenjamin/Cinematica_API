7a13a8fd78344decd86bee3cebd9c4586137fbcf
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-19 19:11:32 +1000

    DELETE route added and tested for removing movie from watchlist

--- src/controllers/watchlist_controller.py
+++ src/controllers/watchlist_controller.py
@@ -61,3 +61,41 @@ def add_movie_to_watchlist(user_id):
     except IntegrityError:
         db.session.rollback()
         return jsonify(message="Movie already in watchlist"), 400
+
+
+@watchlists_bp.route("/", methods=["DELETE"])
+def delete_movie_from_watchlist(user_id):
+    '''DELETE endpoint/handler for removing a movie from a user's watchlist'''
+
+    # Fetch incoming request data
+    data = request.get_json()
+
+    # Get the movie id from the request data
+    movie_id = data.get('movie_id')
+
+    # Get the user's watchlist
+    watchlist = Watchlist.query.filter_by(user_id=user_id).first()
+    if not watchlist:
+        return jsonify(message="No watchlist found for this user"), 404
+
+    # Get the movie
+    movie = Movie.query.get(movie_id)
+    if not movie:
+        return jsonify(message="Movie not found"), 404
+
+    # Check if the movie is in the watchlist
+    if movie not in watchlist.movies:
+        return jsonify(message="Movie not in watchlist"), 400
+
+    # Remove the movie from the watchlist
+    watchlist.movies.remove(movie)
+
+    try:
+        db.session.commit()
+        movie_data = movie_schema.dump(movie)
+
+        # Include the movie data in the response
+        return jsonify(message="Movie sucessfully removed from watchlist", movie=movie_data), 200
+    except Exception as error:
+        db.session.rollback()
+        return jsonify(message=str(error)), 500
