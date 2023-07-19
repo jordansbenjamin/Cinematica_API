d285f3ea2d16456ce45aecce50cb14f84b7d2e43
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-19 18:47:17 +1000

    Cleaned up watchlist controller a bit

--- src/controllers/watchlist_controller.py
+++ src/controllers/watchlist_controller.py
@@ -3,24 +3,13 @@ from flask import Blueprint, request, jsonify
 from sqlalchemy.exc import IntegrityError
 from models.watchlist import Watchlist
 from models.movie import Movie
-from schemas.movie_schema import MovieSchema
+from schemas.movie_schema import movie_schema
 from schemas.watchlist_schema import watchlist_schema, add_movie_to_watchlist_schema
 
 # Initialises flask blueprint for watchlists, prefix is nested and registered with users bp
 watchlists_bp = Blueprint('watchlists', __name__)
 
 
-# @watchlists_bp.route("/", methods=["GET"])
-# def get_watchlists(user_id):
-#     '''GET endpoint/handler for fetching specified users watchlist available in the cinematica app'''
-#     # Queries watchlist instance from the DB
-#     watchlist = Watchlist.query.filter_by(user_id=user_id).first()
-#     # Serialises queried watchlist instance from DB with marshmallow schema into Python DST
-#     result = watchlist_schema.dump(watchlist)
-#     # Returns the serialised data into JSON format for response
-#     return jsonify(result)
-
-# TESTING:
 @watchlists_bp.route("/", methods=["GET"])
 def get_watchlists(user_id):
     '''GET endpoint/handler for fetching specified users watchlist available in the cinematica app'''
@@ -31,16 +20,11 @@ def get_watchlists(user_id):
         return jsonify(message="No watchlist found for this user"), 404
 
     # Serialises queried watchlist instance from DB with marshmallow schema into Python DST
-    print(watchlist.movies)
     result = watchlist_schema.dump(watchlist)
     # Returns the serialised data into JSON format for response
     return jsonify(result), 200
 
-# @watchlists_bp.route("/", methods=["POST"])
-# def add_movie_to_watchlist():
-#     pass
 
-# TESTING:
 @watchlists_bp.route("/", methods=["POST"])
 def add_movie_to_watchlist(user_id):
     '''POST endpoint/handler for adding a movie to a user's watchlist'''
@@ -70,11 +54,8 @@ def add_movie_to_watchlist(user_id):
 
     try:
         db.session.commit()
-        
-        # Create a MovieSchema instance and serialize the movie
-        movie_schema = MovieSchema()
         movie_data = movie_schema.dump(movie)
-        
+
         # Include the movie data in the response
         return jsonify(message="Movie added to watchlist", movie=movie_data), 200
     except IntegrityError:
