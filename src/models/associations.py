889f9f137f685f2bda1deddcbc90175c79a785af
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-20 11:40:45 +1000

    Updated movielog association table in order for controller to work, updates needed similar to watchlist updates

--- src/models/associations.py
+++ src/models/associations.py
@@ -17,4 +17,4 @@ watchlist_movie_association = db.Table('watchlist_movie_association',
 
 
 movielog_movie_association = db.Table('movielog_movie_association', db.Column(
-    'movielog_id', db.Integer, db.ForeignKey('movielogs.id'), nullable=False), db.Column('movie_id', db.Integer, db.ForeignKey('movies.id'), nullable=False))
+    'movielog_id', db.Integer, db.ForeignKey('movielogs.id'), primary_key=True), db.Column('movie_id', db.Integer, db.ForeignKey('movies.id'), primary_key=True), db.Column('date_logged', db.DateTime(), nullable=False, default=datetime.utcnow))
