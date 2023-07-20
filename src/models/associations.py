a8f3547654fc91c4d85ddc927c66d4b514c2ac3d
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-20 11:51:27 +1000

    Added comments on associations table

--- src/models/associations.py
+++ src/models/associations.py
@@ -1,11 +1,13 @@
 from main import db
 from datetime import datetime
 
-# REMOVING THIS FOR NOW
-# watchlist_movie_association = db.Table('watchlist_movie_association', db.Column(
-#     'watchlist_id', db.Integer, db.ForeignKey('watchlists.id'), nullable=False), db.Column('movie_id', db.Integer, db.ForeignKey('movies.id'), nullable=False))
 
-# TESTING
+
+# This is essentially the join table that connects the relationship between the Movies and Watchlists models.
+# It's used because the relationship between these entities is many-to-many, so the association table helps
+# bridge this connection. By bringing in together two foreign keys and making them primary keys in this table,
+# it ensures that each combination of a movie and a watchlist is unique, or in other words, a specific movie
+# can only appear once in a particular watchlist, preventing duplicates of the same movie in the same watchlist.
 watchlist_movie_association = db.Table('watchlist_movie_association',
                                        db.Column('watchlist_id', db.Integer, db.ForeignKey(
                                            'watchlists.id'), primary_key=True),
@@ -15,6 +17,6 @@ watchlist_movie_association = db.Table('watchlist_movie_association',
                                                  nullable=False, default=datetime.utcnow)
                                        )
 
-
+# This is the association (also known as junction or join) table for the Movies and MovieLog models.
 movielog_movie_association = db.Table('movielog_movie_association', db.Column(
     'movielog_id', db.Integer, db.ForeignKey('movielogs.id'), primary_key=True), db.Column('movie_id', db.Integer, db.ForeignKey('movies.id'), primary_key=True), db.Column('date_logged', db.DateTime(), nullable=False, default=datetime.utcnow))
