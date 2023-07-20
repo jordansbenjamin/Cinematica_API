9d584ff34828b99b665fb5a369adceecab295c35
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-21 09:56:28 +1000

    Updated the review model so datetime becomes date instead

--- src/models/review.py
+++ src/models/review.py
@@ -12,7 +12,7 @@ class Review(db.Model):
     review_text = db.Column(db.Text(), nullable=False)
     # The movie review's date
     review_date = db.Column(
-        db.DateTime(), nullable=False, default=datetime.utcnow)
+        db.Date(), nullable=False, default=datetime.utcnow)
     # Fk for user
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     # FK for movie
