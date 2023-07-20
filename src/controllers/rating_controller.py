721cec100b4148b02319c10ba42c71d650d28522
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-20 16:42:35 +1000

    POST route for rating a movie created

--- src/controllers/rating_controller.py
+++ src/controllers/rating_controller.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, jsonify
+from main import db
+from flask import Blueprint, request, jsonify
 from models.rating import Rating
+from models.movie import Movie
 from schemas.rating_schema import rating_schema, ratings_schema
 
 # Initialises flask blueprint for ratings, prefix is nested and registered with users bp
@@ -20,6 +22,35 @@ def get_ratings(user_id):
     # Returns the serialised data into JSON format for response
     return jsonify(response)
 
+
 @ratings_bp.route("/movies/<int:movie_id>/", methods=["POST"])
 def add_movie_rating(user_id, movie_id):
-    pass
+    '''POST endpoint/handler for adding a movie rating for the specified user'''
+
+    rating_body_data = rating_schema.dump(request.json)
+
+    # Get the movie from DB
+    movie = Movie.query.get(movie_id)
+
+    if not movie:
+        return jsonify(message="Movie not found"), 404
+    
+    existing_rating = Rating.query.filter_by(user_id=user_id, movie_id=movie_id).first()
+
+    if existing_rating:
+        return jsonify(message="Movie already rated"), 409
+
+    # Create new rating instance
+    new_rating = Rating(
+        rating_score=rating_body_data["rating_score"],
+        user_id=user_id,
+        movie_id=movie_id
+    )
+
+    # Add and commit new movie rating to DB
+    db.session.add(new_rating)
+    db.session.commit()
+
+    # Return the new rating as a JSON response
+    response = rating_schema.dump(new_rating)
+    return jsonify(response), 201
