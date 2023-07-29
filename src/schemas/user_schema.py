a71ddaca71409af52137b0ba1311b59c1659f976
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-29 18:38:45 +1000

    Cleaned up user schema validation, completed for now

--- src/schemas/user_schema.py
+++ src/schemas/user_schema.py
@@ -1,11 +1,12 @@
 from main import ma
-from marshmallow import validate, pre_load
+from marshmallow import validate
 
 email_error_msg = "Email address provided does not have the correct format"
 username_error_msg = "Username must be between 3 to 25 characters"
 username_regx_error_msg = "Only letters, underscores and numbers are allowed for usernames"
 pw_error_msg = "Password must be between 6 to 25 characters"
 pw_regx_error_msg = "Password cannot contain whitespaces"
+
 
 class UserSchema(ma.Schema):
     class Meta:
@@ -24,21 +25,13 @@ class UserSchema(ma.Schema):
     # Validating email
     email = ma.String(validate=validate.Email(error=email_error_msg))
 
-    # Validating username
+    # Validating username length and characters with regex
     username = ma.String(validate=[validate.Length(
-        min=3, max=25, error=username_error_msg),validate.Regexp(r'^[a-zA-Z0-9_]+$', error=username_regx_error_msg)])
+        min=3, max=25, error=username_error_msg), validate.Regexp(r'^[a-zA-Z0-9_]+$', error=username_regx_error_msg)])
 
-    # Validating password
+    # Validating password length and characters with regex
     password = ma.String(validate=[validate.Length(
         min=6, max=25, error=pw_error_msg), validate.Regexp(r'^\S*$', error=pw_regx_error_msg)])
-        
-        # @pre_load
-        # def trim_input(self, in_data, **kwargs):
-        #     '''Trim leading and trailing whitespaces from input fields.'''
-        #     for key in in_data.keys():
-        #         if isinstance(in_data[key], str):
-        #             in_data[key] = in_data[key].strip()
-        #     return in_data
 
 
 # Singular user schema instance for retreiving a single user
